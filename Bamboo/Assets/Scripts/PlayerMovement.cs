39d7458ee96556548147c01461f1e7bf6253afb6
RyuJinHyeok <101165696+RyuJinHyeok@users.noreply.github.com>
2022-12-23 18:30:06 +0900

    류진혁 플레이어 이동 관련 설정 추가
    플레이어가 화면 밖으로 나가지 못하도록 설정 추가

--- Bamboo/Assets/Scripts/PlayerMovement.cs
+++ Bamboo/Assets/Scripts/PlayerMovement.cs
@@ -16,6 +16,21 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        // 키 입력 시 속도를 주어 오브젝트가 이동하도록 함
+
         rb.velocity = new Vector2(Input.GetAxisRaw("Horizontal") * 2, Input.GetAxisRaw("Vertical") * 2);
+
+
+
+        // 카메라 밖으로 오브젝트가 나가지 못하도록 함
+
+        Vector3 pos = Camera.main.WorldToViewportPoint(transform.position);
+
+        if (pos.x < 0f) pos.x = 0f;
+        if (pos.x > 1f) pos.x = 1f;
+        if (pos.y < 0f) pos.y = 0f;
+        if (pos.y > 1f) pos.y = 1f;
+
+        transform.position = Camera.main.ViewportToWorldPoint(pos);
     }
 }
